f313a3aabb6c69748507c00f047fbd0140a6912c
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 13:34:35 -0600

    Add files via upload

---
+++ PARADAS_MX0_MTY1_copia.js
@@ -0,0 +1,12 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "PARADAS_MX0_MTY1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Guiga Pemex QR", "description": "Autopista Mexico - Queretaro Tramo Palmillas Qr Km 175 32, 76729 Qro.", "layer": "PEMEX_MX0_MTY1", "direccion": "Autopista Mexico - Queretaro Tramo Palmillas Qr Km 175 32, 76729 Qro." }, "geometry": { "type": "Point", "coordinates": [ -100.0983569, 20.4624703, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Mobil Peotillos", "description": "Matehuala - San Luis Potosí 2414, Peyote, S.L.P Peotillos, 78960 Villa Hidalgo, S.L.P.", "layer": "PEMEX_MX0_MTY1", "direccion": "Matehuala - San Luis Potosí 2414, Peyote, S.L.P Peotillos, 78960 Villa Hidalgo, S.L.P." }, "geometry": { "type": "Point", "coordinates": [ -100.6011273, 22.5870359, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "Carrt. Mexico 57 Km 51+000 Mar. Izq, Matehuala, 67920 Dr Arroyo, N.L.", "layer": "PEMEX_MX0_MTY1", "direccion": "Carrt. Mexico 57 Km 51+000 Mar. Izq, Matehuala, 67920 Dr Arroyo, N.L." }, "geometry": { "type": "Point", "coordinates": [ -100.411103, 24.018836, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "50370 Estado de México", "layer": "PEMEX_MX0_MTY1", "direccion": "50370 Estado de México" }, "geometry": { "type": "Point", "coordinates": [ -99.7275173, 20.1298547, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "RETEN FGR SAN ROBERTO", "description": "Matehuala - Monterrey, 67853 Rancho San Jorge Viejo, N.L.", "layer": "PEMEX_MX0_MTY1", "direccion": "Matehuala - Monterrey, 67853 Rancho San Jorge Viejo, N.L." }, "geometry": { "type": "Point", "coordinates": [ -100.2991561, 24.6713409, 0.0 ] } }
+]
+}
---
+++ estaciones_mx0_mty1_copia.js
@@ -0,0 +1,9 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_mty1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "PM6R+9HX", "description": "MTY1", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -100.3085156, 25.7109875, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "19.64220097106514, -99.14250999381147", "description": "MX0", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.1422607, 19.6422227, 0.0 ] } }
+]
+}
